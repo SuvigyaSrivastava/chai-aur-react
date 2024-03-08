90d7e2961732cd51a15f4832d9bd683272cad888
Suvigya <suvigyasrvstva@gmail.com>
2024-03-09 00:56:57 +0530

    first commit

---
+++ 08reactRouter/src/components/User/User.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+function user() {
+const {userid} =useParams()
+  return (
+    <div>user : {userid}</div>
+  )
+}
+
+export default user
